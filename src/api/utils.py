7e88a3c1f27aa8ffc36c5afc040ac3374aa450aa
Ramazan <ramazanui2002@gmail.com>
2023-11-07 01:30:49 +0300

    Create utils file

---
+++ src/api/utils.py
@@ -0,0 +1,18 @@
+from clickhouse_driver import Client
+
+
+client = Client('localhost')
+
+
+def get_popular_words():
+    result = []
+    data, columns = client.execute("select arrayJoin(splitByChar(' ', replaceRegexpAll(text, '[.,]', ' '))) as word, count() as count "
+                                   "from (select text from news) group by word order by count desc limit 1, 100",
+                                   with_column_types=True)
+
+    columns = [col[0] for col in columns]
+    for row in data:
+        json = dict(zip(columns, [value for value in row]))
+        result.append(json)
+
+    return result
